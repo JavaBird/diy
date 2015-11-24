a4daee10b8cc6d071fcc524f3d43ba91c322f8c0
JavaBird <baidu211@vip.qq.com>
2015-11-24 08:08:47 +0800

    导入
    导入

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'name' => 'DIY模板',
+  'author' => 'zhangxh',
+  'dirname' => 'diy',
+  'homepage' => 'http://www.phpcms.cn/',
+  'version' => '1.0',
+  'disable' => 0,
+  'file_explan' => 
+  array (
+    'templates|diy|content' => 
+    array (
+      'category.html' => '文章频道页',
+      'footer.html' => '底部',
+      'header.html' => '顶部',
+      'index.html' => '网站首页',
+      'list.html' => '列表页',
+      'nav.html' => '导航条',
+      'show.html' => '文章内容页',
+    ),
+  ),
+);?>
